99b1b70d2cb78dbc6c2411d70c76609a6f710f93
Asher Surendran <asuren200@DIVWL-L3FMDF8YA.cable.comcast.com>
2012-01-18 14:28:13 -0500

    broke the tests again

--- spec/exercise_spec.rb
+++ spec/exercise_spec.rb
@@ -4,29 +4,29 @@ describe "Integer" do
     
     it "should return the next integer" do
       
-      1.next.should eq 2
-      2.next.should eq 3
+      1.next.should eq 3
+      2.next.should eq 1
       
     end
     
     it "should return the previous integer" do
       
-      1.pred.should eq 0
-      0.pred.should eq -1 
+      1.pred.should eq 1
+      0.pred.should eq 0 
       
     end
     
     it "should tell whether the number is even or not" do
       
-      2.even?.should eq true
-      3.even?.should eq false
+      1.even?.should eq true
+      4.even?.should eq false
     
     end
     
     it "should tell whether the number is odd or not" do
       
-      2.odd?.should eq false
-      1.odd?.should eq true
+      3.odd?.should eq false
+      2.odd?.should eq true
     
     end
   
